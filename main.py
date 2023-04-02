282bb0fc2bfe7923a43c72711412635a1dafc083
mburuanthony <mburuantony@outlook.com>
2023-04-02 14:46:16 +0300

    minor edits

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-import os
 from detector import Detector
+from tts import AudioStraem
+import threading
+import os
 
 
 def main():
-    video_path = 'video\Traffic-27260.mp4'
-    # video_path = 0 # Use video feed from device primary camera
+    video_path = 0
     config_path = os.path.join('model',
                                'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
     model_path = os.path.join('model', 'frozen_inference_graph.pb')
     classes_path = os.path.join('model', 'coco.names')
 
-    detector = Detector(video_path, config_path, model_path, classes_path)
-    detector.readClasses()
-    detector.onVideo()
+    videostream = Detector(video_path, config_path, model_path, classes_path)
+    audiostream = AudioStraem()
+
+    video_thread = threading.Thread(target=videostream.onVideo)
+    audio_thread = threading.Thread(target=audiostream.playstream)
+
+    videostream.readClasses()
+    audiostream.classlabel = videostream.objectclasslabel
+
+    video_thread.start()
+    audio_thread.start()
+
+    video_thread.join()
+    audio_thread.join()
 
 
 if __name__ == '__main__':
